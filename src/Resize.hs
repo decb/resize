388c4148aa9d649eca9d3883178606fa7affcf9e
decb <decb@users.noreply.github.com>
2020-11-23 19:21:02 +0000

    implement edge detection/energy calculation

--- src/Resize.hs
+++ src/Resize.hs
@@ -2,14 +2,31 @@ module Resize
   ( resize
   ) where
 
-import qualified Graphics.Image as Image
+import Control.Monad (join)
+import qualified Graphics.Image as I
 
-resize ::
-     (Image.Array a b c) => Image.Image a b c -> (Int, Int) -> Image.Image a b c
-resize image (wFactor, hFactor) =
-  Image.resize Image.Bilinear Image.Edge (w', h') image
+type Image = I.Image I.VU I.RGB Double
+
+type Pixel = I.Pixel I.RGB Double
+
+resize :: Image -> (Int, Int) -> Image
+resize image (wFactor, hFactor) = energies image
+
+energies :: Image -> Image
+energies image = I.imap energy image
   where
-    (w, h) = Image.dims image
-    w' = scale w wFactor
-    h' = scale h hFactor
-    scale n p = floor (fromIntegral n * fromIntegral p / (100 :: Double))
+    findPixel = I.defaultIndex (I.PixelRGB 1 1 1) image
+    energy (x, y) p =
+      let left = findPixel (x - 1, y)
+          right = findPixel (x + 1, y)
+          above = findPixel (x, y - 1)
+          below = findPixel (x, y + 1)
+          value =
+            sqrt
+              (pixelDifferenceSquared left right +
+               pixelDifferenceSquared above below)
+      in I.PixelRGB value value value
+
+pixelDifferenceSquared :: Pixel -> Pixel -> Double
+pixelDifferenceSquared (I.PixelRGB r0 g0 b0) (I.PixelRGB r1 g1 b1) =
+  join (*) . sum . map abs $ [r0 - r1, g0 - g1, b0 - b1]
