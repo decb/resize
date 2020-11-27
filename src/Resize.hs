a6508a563b0fb8169e63c9544e3552f1116156d6
decb <decb@users.noreply.github.com>
2020-11-27 19:31:05 +0000

    use Vector type for better performance

--- src/Resize.hs
+++ src/Resize.hs
@@ -2,9 +2,9 @@ module Resize
   ( resize
   ) where
 
-import Control.Monad (join)
-import Data.List (elemIndex)
 import Data.Maybe (fromJust)
+import Data.Vector (Vector, (!))
+import qualified Data.Vector as V
 import qualified Graphics.Image as I
 
 type Image c = I.Image I.VU c Double
@@ -34,8 +34,7 @@ energies image = I.imap energy image
       in I.PixelY value
 
 deltaSquared :: Pixel I.RGB -> Pixel I.RGB -> Double
-deltaSquared (I.PixelRGB r0 g0 b0) (I.PixelRGB r1 g1 b1) =
-  join (*) . sum . map abs $ [r0 - r1, g0 - g1, b0 - b1]
+deltaSquared p1 p2 = sum (abs (p1 - p2)) ** 2
 
 data Direction
   = L
@@ -43,18 +42,18 @@ data Direction
   | A
   deriving (Eq)
 
-energyMap :: Image I.Y -> [[(Pixel I.Y, Direction)]]
+energyMap :: Image I.Y -> [Vector (Pixel I.Y, Direction)]
 energyMap image = go [] (reverse (I.toLists image))
   where
     size = I.cols image
     go result [] = result
-    go [] (row:rest) = go [zip row (repeat A)] rest
+    go [] (row:rest) = go [V.fromList (zip row (repeat A))] rest
     go seen@(prev:_) (row:rest) =
-      go (zipWith (combine prev) row [0 ..] : seen) rest
-    combine prev p i =
-      let left = fst (prev !! max (i - 1) 0)
-          above = fst (prev !! i)
-          right = fst (prev !! min (i + 1) (size - 1))
+      go (V.imap (combine prev) (V.fromList row) : seen) rest
+    combine prev i p =
+      let left = fst (prev ! max (i - 1) 0)
+          above = fst (prev ! i)
+          right = fst (prev ! min (i + 1) (size - 1))
       in if left < above && left < right
            then (p + left, L)
            else if above < right
@@ -63,18 +62,18 @@ energyMap image = go [] (reverse (I.toLists image))
 
 removeSeam :: [[Pixel I.RGB]] -> [[Pixel I.RGB]]
 removeSeam image =
-  go [] (fromJust $ elemIndex (minimum firstRow) firstRow) eMap image
+  go [] (fromJust $ V.elemIndex (minimum firstRow) firstRow) eMap image
   where
     eMap = energyMap (energies (I.fromLists image))
-    firstRow = map fst (head eMap)
+    firstRow = fmap fst (head eMap)
     go result n (e:es) (row:rs) =
-      let (_, direction) = e !! n
-          n'
-            | direction == L = n - 1
-            | direction == R = n + 1
-            | otherwise = n
+      let n' =
+            case snd (e ! n) of
+              L -> n - 1
+              R -> n + 1
+              A -> n
       in go (deleteIndex n row : result) n' es rs
-    go result _ _ _ = result
+    go result _ _ _ = reverse result
 
 deleteIndex :: Int -> [a] -> [a]
 deleteIndex i xs = ys ++ drop 1 zs
